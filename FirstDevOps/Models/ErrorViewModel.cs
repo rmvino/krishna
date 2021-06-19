51e5c844bda0b0b6d749f9faf2b46355401ed94f
rmvino <86161100+rmvino@users.noreply.github.com>
2021-06-19 21:43:13 +0530

    Add files via upload

---
+++ FirstDevOps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstDevOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
